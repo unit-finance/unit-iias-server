dc2ed662178145e223f22170f163c80b659134d5
Gai Tamir <gai@unit-finance.com>
2021-11-02 15:10:00 +0200

    convert amount to integer

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import {
 } from "./utils"
 
 import moment from "moment"
-import {minBy, toInteger, flatten} from "lodash"
+import {minBy, toInteger, flatten, parseInt} from "lodash"
 
 const app = express()
 
@@ -41,7 +41,7 @@ app.post("/webhooks", async (req, res) => {
 async function handlePendingAuthorizationRequest(req: express.Request) {
     const authorizationRequestId = req.body.data[0].relationships.authorizationRequest.data.id
     const partialApprovalAllowed = req.body.data[0].attributes.partialApprovalAllowed
-    const totalHealthcareAmount = req.body.data[0].attributes.healthCareAmounts != null ? req.body.data[0].attributes.healthCareAmounts.totalHealthcareAmount : 0
+    const totalHealthcareAmount = req.body.data[0].attributes.healthCareAmounts != null ? toInteger(req.body.data[0].attributes.healthCareAmounts.totalHealthcareAmount) : 0
 
     const response = await axios.post(`${UNIT_BASE_API_URL}/authorization-requests/${authorizationRequestId}/approve`, 
         getApproveRequestBody(partialApprovalAllowed, totalHealthcareAmount), {
